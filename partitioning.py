89f7d0b150787da9cf22a06cc0692ca5e93da88a
Andreus Maximus <a.faesen@gmail.com>
2021-12-03 15:06:26 +0100

    Initial commit

---
+++ partitioning.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+	
+	
+#add partition
+#Check if line intersects with partition
+#0 if it intersects
+#1 if it fits and is added
+def addLine2Partition(line, partition):
+	return 0
+
+#creates a new partition 
+#0 is failed
+#1 is created succesfully
+def createNewPartition():
+	return 0
+
+#splits partition over two partition
+#randomly splits a partition in 2
+def splitPartition():
+	return 0
+	
+#check if two lines cross each other
+#1 if intersects
+#0 if it doesnt intersect
+def checkIntersection(line_1, line_2):
+	return 0
+
+def main(args):
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
